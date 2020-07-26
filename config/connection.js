890b4599fb955a87af160e2db411b043d080d846
Erik De Luna <codingerik1.gmail.com>
2020-07-25 23:11:41 -0500

    adding hoisted var

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,10 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-
+var connection
 if (process.env.JAWSD_URL) {
   connection = mysql.createConnection(process.env.JAWSD_URL); 
-}else{   var connection = mysql.createConnection({
+}else{   
+    connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
