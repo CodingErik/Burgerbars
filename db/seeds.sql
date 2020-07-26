62984391d46594c5eea1f6645db736e01077c3e7
Erik De Luna <codingerik1.gmail.com>
2020-07-25 20:08:45 -0500

    updated seed values

--- db/seeds.sql
+++ db/seeds.sql
@@ -8,9 +8,10 @@ INSERT INTO cats (name) VALUES ('Owens');
 
 
 -- seed for the burgers table
+
 -- INSERT INTO burgers (name) VALUES ('bacon Cheese');
 -- INSERT INTO burgers (name) VALUES ('Bobbi sandwich');
--- INSERT INTO burgers (name, sleepy) VALUES ('Sylvester calzone', true);
--- INSERT INTO burgers (name, sleepy) VALUES ('Marilyn quesadilla burger', true);
--- INSERT INTO burgers (name, sleepy) VALUES ('Joe half pounder', true);
+-- INSERT INTO burgers (name, devoured) VALUES ('Sylvester calzone', true);
+-- INSERT INTO burgers (name, devoured) VALUES ('Marilyn quesadilla burger', true);
+-- INSERT INTO burgers (name, devoured) VALUES ('Joe half pounder', true);
 -- INSERT INTO burgers (name) VALUES ('Owens chargrilled burger');
